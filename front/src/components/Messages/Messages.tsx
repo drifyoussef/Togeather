ced1ce450c416310576b507ea4456597e42b63af
youssefdrif <youssef.drif1@outlook.com>
2024-06-25 17:03:32 +0200

    fix messages css & tsx

--- front/src/components/Messages/Messages.tsx
+++ front/src/components/Messages/Messages.tsx
@@ -14,8 +14,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -27,8 +26,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -40,8 +38,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -53,8 +50,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -66,8 +62,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -79,8 +74,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
         <div className="match-box">
@@ -92,21 +86,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
-          </p>
-        </div>
-        <div className="match-box">
-          <div className="header-match-box">
-            <div className="circle-match">L</div>
-            <div className="header-text">
-              <p>Laura, 18 ans</p>
-              <p>Asiatique</p>
-            </div>
-          </div>
-          <p className="preview-message-box">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
       </div>
@@ -186,12 +166,12 @@ export default function Messages() {
               <span className="time">10:31 AM</span>
             </div>
           </div>
-          <div className="bubble-type-div">
-            <div className="bubble-type">
-              <p>I'm good, thanks! How about you?</p>
-            </div>
-            <button className="send-message">Envoyer</button>
+        </div>
+        <div className="bubble-type-div">
+          <div className="bubble-type">
+            <p>I'm good, thanks! How about you?</p>
           </div>
+          <button className="send-message">Envoyer</button>
         </div>
       </div>
       <div className="profile-match">
@@ -204,8 +184,7 @@ export default function Messages() {
             </div>
           </div>
           <p className="profile-bio">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-            quo!
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, quo!
           </p>
         </div>
       </div>
