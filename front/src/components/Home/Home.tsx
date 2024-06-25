6ba4cd9461593fde1d2df5547c3b3686df76e312
youssefdrif <youssef.drif1@outlook.com>
2024-06-25 16:10:50 +0200

    add logo for categories

--- front/src/components/Home/Home.tsx
+++ front/src/components/Home/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Home.css";
 import Card from "../Card/Card";
+import { BiSolidSushi } from "react-icons/bi";
+import { FaPizzaSlice, FaHamburger, FaIceCream } from "react-icons/fa";
+import { GiChickenOven, GiTacos } from "react-icons/gi";
+import { LuSandwich } from "react-icons/lu";
+import { RiDrinks2Fill } from "react-icons/ri";
 
 export default function Home() {
   return (
@@ -14,35 +19,35 @@ export default function Home() {
           <p className="p3-home">Catégories de restaurant</p>
           <div className="parent">
             <div className="div1">
-              <div className="circle"></div>
+              <div className="circle"><BiSolidSushi className="icon-category" /></div>
               <p className="p-category">Asiatique</p>
             </div>
             <div className="div2">
-              <div className="circle"></div>
+              <div className="circle"><FaPizzaSlice className="icon-category" /></div>
               <p className="p-category">Pizza</p>
             </div>
             <div className="div3">
-              <div className="circle"></div>
+              <div className="circle"><GiChickenOven className="icon-category" /></div>
               <p className="p-category">Poulet</p>
             </div>
             <div className="div4">
-              <div className="circle"></div>
+              <div className="circle"><LuSandwich className="icon-category" /></div>
               <p className="p-category">Sandwich</p>
             </div>
             <div className="div5">
-              <div className="circle"></div>
+              <div className="circle"><GiTacos className="icon-category" /></div>
               <p className="p-category">Mexicain</p>
             </div>
             <div className="div6">
-              <div className="circle"></div>
-              <p className="p-category">French tacos</p>
+              <div className="circle"><FaHamburger className="icon-category" /></div>
+              <p className="p-category">Burger</p>
             </div>
             <div className="div7">
-              <div className="circle"></div>
+              <div className="circle"><FaIceCream className="icon-category" /></div>
               <p className="p-category">Glaces</p>
             </div>
             <div className="div8">
-              <div className="circle"></div>
+              <div className="circle"><RiDrinks2Fill className="icon-category" /></div>
               <p className="p-category">Boissons</p>
             </div>
           </div>
@@ -84,28 +89,76 @@ export default function Home() {
         <p className="p-match">Mes matchs</p>
         <div className="parent">
           <div className="div1">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div2">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div3">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div4">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div5">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div6">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div7">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
           <div className="div8">
-            <div className="circle"></div>
+            <div className="circle">
+              <img
+                className="image-match"
+                src="https://architecture.ou.edu/wp-content/uploads/2018/07/ANGELAPERSON-1447-300x300.jpg"
+                alt="match-person"
+              />
+            </div>
           </div>
         </div>
       </div>
