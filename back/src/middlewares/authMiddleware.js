098f388e4c38c1b3824d623393a0974489c24608
youssefdrif <youssef.drif1@outlook.com>
2024-06-26 11:38:02 +0200

    jwt instead of session

--- back/src/middlewares/authMiddleware.js
+++ back/src/middlewares/authMiddleware.js
@@ -1,12 +1,29 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = async (req, res, next) => {
-    const userFind = await User.findById(req.session.userId)
-    try {
-        if (!userFind)
-            return res.redirect('/')
-    } catch (error) {
-        console.log(`erreur sur authmiddleware => ${error}`);
-    }
-    next()
+    //Recupérer le token qui sera dans le req.headers.Authorization (faudra enlever Bearer)
+    const token = req.headers.authorization;
+    
+	if(token) {
+		const tokenWithoutBearer = token.replace("Bearer ", "");
+		try {
+            //Utiliser jwt pour decordér le token
+            const privateKey = fs.readFileSync(path.join(process.cwd(), "private.key"));
+			const decoded = jwt.verify(tokenWithoutBearer, privateKey);
+            //On pourra utiliser token._id
+			req.user = decoded;
+            req.session.userId = decoded._id;
+			next();
+		}
+		catch(err) {
+            console.log("err", err)
+			res.status(401).send('Unauthorized');
+		}
+	}
+	else {
+		res.status(401).send('Unauthorized');
+	}
 }
--- front/src/components/Profile/Profile.tsx
+++ front/src/components/Profile/Profile.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
-import './Profile.css';
+import React, { useEffect, useState } from "react";
+import "./Profile.css";
+import { UserModel } from "../../models/User.model";
 
 const Profile: React.FC = () => {
-    const changeSettings = () => {
-        alert('Change settings clicked');
-    };
+  const [user, setUser] = useState<UserModel>();
 
-    return (
-      <div className='div-profile'>
-        <div className="profileCard">
-            <div className="profileImage"></div>
-            <div className="profileInfo">
-                <h2>John Doe</h2>
-                <p>Web Developer</p>
-                <button onClick={changeSettings}>Change Settings</button>
-            </div>
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    fetch("http://localhost:4000/auth/user", {
+      headers: {
+        "Authorization":`Bearer ${token}`
+      },
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message) {
+          console.error(data.message);
+        } else {
+          setUser(data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      });
+  }, []);
+
+  const changeSettings = () => {
+    alert("Change settings clicked");
+  };
+
+  return (
+    <div className="div-profile">
+      <div className="profileCard">
+        <div className="profileImage"></div>
+        <div className="profileInfo">
+          {user ? (
+            <h1>
+              {user.firstname} {user.name}
+            </h1>
+          ) : (
+            <h1>Loading...</h1>
+          )}
+          <p>Web Developer</p>
+          <button onClick={changeSettings}>Change Settings</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Profile;
