eba02e29fe80be4aad81bebb708ff7cda648b43d
youssefdrif <youssef.drif1@outlook.com>
2024-07-12 12:57:51 +0200

    fix docker compose

--- back/index.js
+++ back/index.js
@@ -32,7 +32,7 @@ app.use('*', (req, res, next) => {
 //Définition des CORS Middleware 
 app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-    res.setHeader("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
+    res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
