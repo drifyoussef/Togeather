a808b552bb8e1b4b4470f404ff86fec73a6d32d3
drifyoussef <youssef.drif@beinstore.fr>
2025-05-21 02:13:55 +0200

    fix docker frontend refresh and connection page

--- front/src/components/Connection/Connection.tsx
+++ front/src/components/Connection/Connection.tsx
@@ -1,46 +1,39 @@
 import React from "react";
 import "./Connection.css";
-import { FaMessage } from "react-icons/fa6";
-import { FaUser } from "react-icons/fa";
-import { MdRestaurant } from "react-icons/md";
-import { TbPremiumRights } from "react-icons/tb";
+import { FaArrowRightLong } from "react-icons/fa6";
+import connectionBackground from "../../images/connection_background.jpeg";
 
 export default function Connexion() {
-
-  const LoginRedirect = () => {
-    // Rediriger vers la page de connexion
-    window.location.href = "/auth/login";
-  };
-  
   return (
-    <div className="div-container-connection">
+    <div>
       <div className="top-container">
         <p className="p1-connection">Let's eat together</p>
         <p className="p2-connection">Mangeons ensemble</p>
       </div>
-      <div className="content-container">
-        <p className="p3-connection">Togeather est un site de rencontre amicale permettant de sociabiliser autour d'un repas à proximité de votre localisation !</p>
-        <div className="icons-container">
-          <div className="message-container-connexion">
-            <FaMessage className="message-icon" />
-            <p className="text-icons">Discutez avec les personnes que vous likez</p>
-          </div>
-          <div className="user-container-connexion">
-            <FaUser className="user-icon" />
-            <p className="text-icons">Modifiez vos informations quand vous le voulez</p>
-          </div>
-          <div className="restaurant-container-connexion">
-            <MdRestaurant className="restaurant-icon" />
-            <p className="text-icons">Decouvrez de nouveaux restaurants</p>
-          </div>
-          <div className="premium-container-connexion">
-            <TbPremiumRights className="premium-icon" />
-            <p className="text-icons">Achetez le premium pour avoir des likes illimités</p>
+      <div className="div-container-connection">
+        <div className="content-container">
+          <p className="text-meeting-connection">
+            Rencontrez des gens autour d'un bon repas
+          </p>
+          <p className="text-meeting-connection-lower">
+            Discutez avec des personnes pour faire des rencontres amicales
+          </p>
+          <div className="bottom-container">
+            <button className="start-button">
+              Commencer
+              <FaArrowRightLong />
+            </button>
+            <button className="tutorial-button">Tutoriel</button>
           </div>
         </div>
-        <p className="connection">
-          <button className="connection-button" onClick={LoginRedirect}>Se connecter</button> pour rencontrer des gens
-        </p>
+        <div className="image-container">
+          <div className="image-bg"></div>
+          <img
+            className="image-connection"
+            src={connectionBackground}
+            alt="Connection"
+          />
+        </div>
       </div>
     </div>
   );
