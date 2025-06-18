590bff18c0ca40d7dce1867918c2859e8d643543
drifyoussef <youssef.drif@beinstore.fr>
2025-06-18 20:18:39 +0200

    fix storeUser

--- back/src/controllers/storeUser.js
+++ back/src/controllers/storeUser.js
@@ -9,16 +9,27 @@ module.exports = async (req, res) => {
         res.redirect('/')
     } catch (error) {
         console.log(JSON.stringify(error));
+
         // Gestion de l'unicité de l'email
         if (error.code === 11000 && error.keyPattern && error.keyPattern.email) {
             // Erreur d'unicité sur l'email
             return res.status(400).json({ message: "Cette adresse mail est déjà associée à un compte." });
         }
+
+        // Cas 2 : erreur de validation Mongoose (ValidatorError sur unique)
+        if (
+        error.errors &&
+        error.errors.email &&
+        (error.errors.email.kind === "unique" || error.errors.email.type === "unique")
+        ) {
+        return res.status(400).json({ message: "Cette adresse mail est déjà associée à un compte." });
+        }
+
+
        const validationErrors = Object.keys(error.errors || {}).map(key => error.errors[key].message)
         res.status(400).json({ message: validationErrors.join(', ') || "Erreur lors de l'inscription." });
         //req.session.validationErrors = validationErrors;
         res.redirect('/auth/register');
         console.log(`Create User error ${error}`);
     }
-
 }
