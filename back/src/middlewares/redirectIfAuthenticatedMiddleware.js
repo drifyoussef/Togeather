3d8a1f05c58e9979b121bfff5d34067cf969bace
drifyoussef <youssef.drif@beinstore.fr>
2025-08-05 10:52:31 +0200

    change middleware add jwt and token if already logged in

--- back/src/middlewares/redirectIfAuthenticatedMiddleware.js
+++ back/src/middlewares/redirectIfAuthenticatedMiddleware.js
@@ -1,7 +1,14 @@
 module.exports = (req, res, next) => {
     // Si l'utilisateur est connecté, on le redirige vers la page d'accueil
     if (req.session.userId) {
-        return res.status(200).json({ alreadyLoggedIn: true });
-    }
+  // Génère le token ici si besoin
+  const privateKey = fs.readFileSync(
+    path.join(appRoot.path, "private.key")
+  );
+  const token = jwt.sign({ _id: req.session.userId }, privateKey, {
+    algorithm: "RS256",
+  });
+  return res.status(200).json({ alreadyLoggedIn: true, token: token });
+}
     next()
 }
