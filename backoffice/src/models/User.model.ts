aad708d736fabe5185c5844b03e227e0620fdf8b
drifyoussef <youssef.drif@beinstore.fr>
2025-09-03 22:31:08 +0200

    fix usermodel errors bo

--- backoffice/src/models/User.model.ts
+++ backoffice/src/models/User.model.ts
@@ -3,4 +3,21 @@ export interface UserModel {
   _id: string;
   email: string;
   password: string;
+  firstname?: string;
+  name?: string;
+  age?: number;
+  job?: string;
+  passions?: string[];
+  userGender?: string;
+  preferredGender?: string;
+  favoriteCategory?: string;
+  imageUrl?: string;
+  isPremium?: boolean;
+  isEmailConfirmed?: boolean;
+  isBanned?: boolean;
+  banReason?: string;
+  banEnd?: Date;
+  likedBy?: string[];
+  mutualMatches?: string[];
+  isMutual?: boolean;
 }
