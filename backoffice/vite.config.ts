d082e0b5f98e4638068dfaa79222d19e43396965
drifyoussef <youssef.drif@beinstore.fr>
2025-09-06 18:10:58 +0200

    fix error mime type

--- backoffice/vite.config.ts
+++ backoffice/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/backoffice/' : '/',
+  base: command === 'serve' ? '/' : '/backoffice/',
   server: {
     port: 3001,
     host: true,
