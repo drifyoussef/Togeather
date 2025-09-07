2de5395636ae5c34aa390c7bd9d44cbf05ae50b6
drifyoussef <youssef.drif@beinstore.fr>
2025-09-07 02:53:48 +0200

    fix footer messages

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "./Footer.css";
+import { useFetchUsers } from "../../hooks/useFetchUsers";
+import Swal from "sweetalert2";
 
-const Footer: React.FC = () => (
-  <footer className="footer">
-    <div className="footer-content">
-      <span>
-        &copy; {new Date().getFullYear()} Togeather. Tous droits réservés.
-      </span>
-      <div className="footer-links">
+const Footer: React.FC = () => {
+  const { mutualMatches } = useFetchUsers();
+  const navigate = useNavigate();
+
+  const handleClickMessages = () => {
+    if (mutualMatches.length > 0) {
+      const firstUser = mutualMatches[0];
+      localStorage.setItem("selectedUserId", firstUser._id);
+      navigate(`/messages/${firstUser._id}`);
+      //console.log(firstUser._id, "firstUser ID");
+    } else {
+      console.error("No mutual matches found");
+      Swal.fire({
+        icon: "error",
+        title: "Il y a un problème...",
+        text: "Vous n'avez pas de matchs pour le moment",
+        confirmButtonColor: "#AD0051",
+      });
+      navigate("/");
+    }
+  };
+
+  return (
+    <footer className="footer">
+      <div className="footer-content">
         <span>
-          <a href="/cgv">Conditions Générales de Vente</a> |{" "}
-          <a href="/cgu">Conditions Générales d'Utilisation</a>
+          &copy; {new Date().getFullYear()} Togeather. Tous droits réservés.
         </span>
-        <span>
-          <a href="/">Accueil</a> |{" "}
-          <a href="/premium">Premium</a> |{" "}
-          <a href="/browse">Parcourir</a> |{" "}
-          <a href="/messages">Mes messages</a> |{" "}
-          <a href="/profile">Mon profil</a>
-        </span>
+        <div className="footer-links">
+          <span>
+            <a href="/cgv">Conditions Générales de Vente</a> |{" "}
+            <a href="/cgu">Conditions Générales d'Utilisation</a>
+          </span>
+          <span>
+            <a href="/">Accueil</a> |{" "}
+            <a href="/premium">Premium</a> |{" "}
+            <a href="/browse">Parcourir</a> |{" "}
+            <button 
+              onClick={handleClickMessages}
+              className="footer-link-button"
+            >
+              Mes messages
+            </button>{" "}
+            |{" "}
+            <a href="/profile">Mon profil</a>
+          </span>
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
+
